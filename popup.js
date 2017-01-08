915e5570cebfc4097e0d876c8c2b53737f28d22f
Christopher <czaremba101@gmail.com>
2017-01-08 09:42:48 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -10,13 +10,13 @@ document.addEventListener('DOMContentLoaded', function() {
         saveRatings('');
     });
 });
-
+chrome.extension.sendRequest({redirect: 'google.com'});
 var pattern=/\bBlocked/;
 var viewtext_base_url = "http://viewtext.org/article?url=";
 var newurl;
 if (pattern.test(window.document.title)) // if it matches pattern defined above
 {
-  newurl = viewtext_base_url + encodeURIComponent(window.location.href);
-  chrome.extension.sendRequest({redirect: newurl}); // send message to redirect
+  //newurl = viewtext_base_url + encodeURIComponent(window.location.href);
+   // send message to redirect
 
 }
